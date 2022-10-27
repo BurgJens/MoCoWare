867e5e53c88622e42a22df4be547ea0c2a786ce4
BurgJens <91873641+BurgJens@users.noreply.github.com>
2022-10-27 22:04:34 +0200

    test some stuff

--- testjens/src/main/java/de/testjens/MainActivity.kt
+++ testjens/src/main/java/de/testjens/MainActivity.kt
@@ -1,14 +1,24 @@
 package de.testjens
 
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import de.testjens.ui.theme.MoCoWareTheme
 
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    UserEntry("Android", 10)
                 }
             }
         }
@@ -34,10 +44,30 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun UserEntry(name: String, rounds: Int){
+    Row(){
+        Image(painterResource(R.drawable.ic_baseline_account_box_24),
+            "bla",
+            Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+        Column() {
+            Text(text = name)
+            Row() {
+              repeat(rounds) {
+                Image(painterResource(R.drawable.ic_baseline_square_24), "odgjodgj")
+              }
+            }
+        }
+    }   
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MoCoWareTheme {
-        Greeting("Android")
+        UserEntry("Jürgen",10)
     }
 }
