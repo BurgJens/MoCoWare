d6241aa5335037da343f7d5197eba8f3e59bfb61
BurgJens <91873641+BurgJens@users.noreply.github.com>
2023-02-09 19:56:17 +0100

    set compose kotlin compilr vrs. to 1.4.1 and kotlin vers. to 1.8.0, fixes .random functions

--- app/src/main/java/de/mocoware/model/ActiveGame.kt
+++ app/src/main/java/de/mocoware/model/ActiveGame.kt
@@ -1,50 +1,51 @@
-package de.mocoware.model
-
-
-import de.mocoware.model.minigames.MiniGame2
-import de.mocoware.model.minigames.MiniGameListe
-import kotlin.random.Random
-
-object ActiveGame{
-
-    var isActive = false
-
-    private val name = ""
-
-    private var gameId = ""
-
-    private val miniGames = mutableListOf<MiniGame2>()
-
-    init {
-        addMinigames(10)
-    }
-
-    private fun generateGameId() : String{
-        var newID = ""
-        val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        repeat(4){
-            newID += charPool[Random.nextInt(charPool.size)]
-        }
-        return newID
-    }
-
-    fun getID() : String{
-        return gameId
-    }
-
-    fun getName() : String{
-        return name
-    }
-
-    fun addMinigames(amount: Int){
-        miniGames.clear()
-        val miniGameEnumValues = MiniGameEnum.values()
-        val nextMinigame = miniGameEnumValues[Random.nextInt(MiniGameEnum.values().size)]
-        repeat(amount){
-            when(nextMinigame){
-                MiniGameEnum.MGannoyingButtons -> miniGames.add(MiniGameListe().zufallSpiel())
-            }
-        }
-    }
-
-}
+//package de.mocoware.model
+//
+//
+//import de.mocoware.model.minigames.MiniGame2
+//import de.mocoware.model.minigames.MiniGameListe
+//import kotlin.random.Random
+//
+//object ActiveGame{
+//
+//    var isActive = false
+//
+//    private val name = ""
+//
+//    private var gameId = ""
+//
+//    private val miniGames = mutableListOf<MiniGame2>()
+//
+//    init {
+//        addMinigames(10)
+//    }
+//
+//    private fun generateGameId() : String{
+//        var newID = ""
+//        val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+//        repeat(4){
+//            newID += charPool[Random.nextInt(charPool.size)]
+//        }
+//        return newID
+//    }
+//
+//    fun getID() : String{
+//        return gameId
+//    }
+//
+//    fun getName() : String{
+//        return name
+//    }
+//
+//    fun addMinigames(amount: Int){
+//        miniGames.clear()
+//        val miniGameEnumValues = MiniGameEnum.values()
+//        val nextMinigame = miniGameEnumValues[Random.nextInt(MiniGameEnum.values().size)]
+//        repeat(amount){
+//            when(nextMinigame){
+//                MiniGameEnum.MGannoyingButtons -> miniGames.add(MiniGameListe().zufallSpiel())
+//                else -> {}
+//            }
+//        }
+//    }
+//
+//}
